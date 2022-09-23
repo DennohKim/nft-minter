7fd1220cc0aaa31e676c8fb48cf3e62850ea84cf
Dennis Kimathi <kimathidennis38@gmail.com>
2022-09-23 10:30:32 +0300

    feat: implement createListing function

--- hardhat/contracts/NFTMarketplace.sol
+++ hardhat/contracts/NFTMarketplace.sol
@@ -7,7 +7,39 @@ contract NFTMarketplace {
     struct Listing {
     uint256 price;
     address seller;
+    }
+
+    mapping(address => mapping(uint256 => Listing)) public listings;
+
+    function createListing(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 price
+    ) external {
+        // Cannot create a listing to sell NFT for < 0 ETH
+        require(price > 0, "MRKT: Price must be > 0");
+
+        // If listing already existed, listing.price != 0
+        require(
+            listings[nftAddress][tokenId].price == 0,
+            "MRKT: Already listed"
+        );
+
+        // Check caller is owner of NFT, and has approved
+        // the marketplace contract to transfer on their behalf
+        IERC721 nftContract = IERC721(nftAddress);
+        require(nftContract.ownerOf(tokenId) == msg.sender, "MRKT: Not the owner");
+        require(
+            nftContract.isApprovedForAll(msg.sender, address(this)) ||
+                nftContract.getApproved(tokenId) == address(this),
+            "MRKT: No approval for NFT"
+        );
+
+        // Add the listing to our mapping
+        listings[nftAddress][tokenId] = Listing({
+            price: price,
+            seller: msg.sender
+        });
+    }
 }
 
-mapping(address => mapping(uint256 => Listing)) public listings;
-}
