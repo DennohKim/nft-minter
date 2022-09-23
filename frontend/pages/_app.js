1d5130d183e9585f578422eb7c248fdc01c1a4cd
Dennis Kimathi <kimathidennis38@gmail.com>
2022-09-23 14:14:28 +0300

    feat: create configuration object for alfajores chain

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,30 @@
-import '../styles/globals.css'
+import "@rainbow-me/rainbowkit/styles.css";
+import "../styles/globals.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+
+const celoChain = {
+  id: 44787,
+  name: "Celo Alfajores Testnet",
+  network: "alfajores",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Celo",
+    symbol: "CELO",
+  },
+  rpcUrls: {
+    default: "https://alfajores-forno.celo-testnet.org",
+  },
+  blockExplorers: {
+    default: {
+      name: "CeloScan",
+      url: "https://alfajores.celoscan.io",
+    },
+  },
+  testnet: true,
+};
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
